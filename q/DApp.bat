87dbb8466059d1f4c60ae24c66b46291231e2bb6
Dima Mezhevich <octadim@gmail.com>
2024-01-18 16:39:33 +0300

    +quick command: Quick comman DApp.bat was added

--- q/DApp.bat
+++ q/DApp.bat
@@ -1,7 +1,17 @@
-rem cd applications
-rem python.exe ../manage.py startapp %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem cd ..
+@echo off
+if exist applications (
+    @echo on
+    cd applications
+    python.exe ../manage.py startapp %1 %2 %3 %4 %5 %6 %7 %8 %9
+    cd ..
+    @echo off
+)
 
-rem cd apps
-rem python.exe ../manage.py startapp %1 %2 %3 %4 %5 %6 %7 %8 %9
-rem cd ..
+@echo off
+if exist apps (
+    @echo on
+    cd apps
+    python.exe ../manage.py startapp %1 %2 %3 %4 %5 %6 %7 %8 %9
+    cd ..
+        @echo off
+)
