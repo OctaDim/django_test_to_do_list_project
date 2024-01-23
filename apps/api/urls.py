f7b243912e9f673882e1ed716e12360669a33019
Dima Mezhevich <octadim@gmail.com>
2024-01-24 02:48:04 +0300

    intermediate night closing commit

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -23,10 +23,10 @@ from apps.api.views.views_model_view_set import (StatusViewSet,
 app_name = "api"
 
 urlpatterns = [
-    # views via ViewSet (samples)
-    # for ModelViewSet classes router is used, see bellow
+    # views via ViewSet (samples):
+    # --> for ModelViewSet classes DefaultRouter is used (see section bellow)
 
-    # views via GenericViews (samples)
+    # views via GenericViews (samples):
     path("generics/task/<int:task_id>", TaskByIdGenericRetrieve.as_view(), name="get-task-by-id"),
     path("generics/task_with_subtasks/<int:task_id>", TaskByIdWithSubtasksGenericRetrieve.as_view(), name="get-task-by-id-with-subtasks"),
     path("generics/all_tasks", AllTasksWithSubtasksGenericList.as_view(), name="get-all-tasks"),
@@ -34,15 +34,15 @@ urlpatterns = [
     path("generics/subtask/<int:subtask_id>", SubtaskByIdGenericRetrieveUpdateDelete.as_view(), name="get-subtask-by-id"),
     path("generics/filtered_tasks/", TasksFilteredGenericListCreate.as_view(), name="get-filtered-tasks"),
 
-    # views via ApiViews classes (samples)
+    # views via ApiViews classes (samples):
     path("api_views/tasks/", TasksApiViews.as_view(), name="tasks-api-view"),
 
-    # views via functions (samples)
+    # views via functions (samples):
     path("function/all_tasks", get_all_tasks, name="get-all-tasks"),
     path("function/create_task", create_new_task, name="create-new-task"),
 ]
 
-# #################### FOR MODEL VIEW SET ONLY #########################
+# #################### FOR MODEL VIEW SET ONLY: ########################
 router = DefaultRouter()  # For ModelViewSet only
 router.register(r"model_view_set/category", CategoryViewSet)  # regular expression is used r"category". WITHOT '/' at the end of url!!!
 router.register(r"model_view_set/status", StatusViewSet)  # regular expression is used r"status". WITHOT '/' at the end of url!!!
