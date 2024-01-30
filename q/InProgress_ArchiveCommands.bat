66b6ab2c8875ad222f4c0640cb551c6b3b5220ef
Dima Mezhevich <octadim@gmail.com>
2024-01-30 21:33:35 +0300

    initial commit: branch: api_user_registration

---
+++ q/InProgress_ArchiveCommands.bat
@@ -0,0 +1,8 @@
+python -m venv venv
+.venv\Scripts\activate
+
+pip freeze > "requirements.txt"
+pip uninstall -r "requirements.txt"
+pip uninstall -r "requirements.txt" -y
+
+pip install -r "requirements.txt"
