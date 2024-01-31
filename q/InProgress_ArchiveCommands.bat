3ff09ba2b3564fcc9519d5368649aa2987e81f20
Dima Mezhevich <octadim@gmail.com>
2024-01-31 15:38:49 +0300

    Quick Command.bat edited

--- q/InProgress_ArchiveCommands.bat
+++ q/InProgress_ArchiveCommands.bat
@@ -12,5 +12,6 @@ pip install django-enumchoicefield
 git checkout my_branch rem Go to the branch my_branch
 git rebase master rem Rebasing all commits to the master
 git checkout master rem Go to the branch master
-git merge my_branch —ff rem (fast forward merge) To merge with my_branch WITHOUT merging commit
-git merge my_branch —no-ff rem (no fast forward merge) To merge with my_branch WITH merging commit
+git merge my_branch —ff rem (fast forward merge) rem To merge with my_branch WITHOUT merging commit
+git merge my_branch —no-ff rem (no fast forward merge) rem To merge with my_branch WITH merging commit
+git branch -d my_branch rem Deleting feat branch
