520e463414373bcc740c07554c9ffa7a0732eb8e
Dima Mezhevich <octadim@gmail.com>
2024-01-31 13:54:57 +0300

    +feat: BooleanField(from rest_framework.fields) overrided to get default value=True(checked flag)

--- apps/api/enums.py
+++ apps/api/enums.py
@@ -1,6 +1,12 @@
 from enumchoicefield import ChoiceEnum
 from django.utils.translation import gettext_lazy
+from rest_framework.fields import BooleanField
 
 class YesNoChoiceEnum(ChoiceEnum):
     Yes = True
     No = False
+
+
+class CustomTrueByDeafaultBooleanField(BooleanField):
+    default_empty_html = True
+    initial = True
