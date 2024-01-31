e13da515ee10aa3feb0f7c643f2f004b9ee418b5
Dima Mezhevich <octadim@gmail.com>
2024-01-31 14:27:19 +0300

    Quick Command.bat edited

--- q/InProgress_ArchiveCommands.bat
+++ q/InProgress_ArchiveCommands.bat
@@ -8,3 +8,9 @@ pip uninstall -r "requirements.txt" -y
 pip install -r "requirements.txt"
 
 pip install django-enumchoicefield
+
+git checkout my_branch rem Go to the branch my_branch
+git rebase master rem Rebasing all commits to the master
+git checkout master rem Go to the branch master
+git merge my_branch —ff rem (fast forward merge) To merge with my_branch WITHOUT merging commit
+git merge my_branch —no-ff rem (no fast forward merge) To merge with my_branch WITH merging commit
