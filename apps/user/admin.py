a21297d4d0bce737208a9f84ecb36c3577fe3fe5
Dima Mezhevich <octadim@gmail.com>
2024-01-31 21:05:52 +0300

    +feat: Added custom User model to the admin panel

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from apps.user.models import User
 
 # Register your models here.
+
+@admin.register(User)
+# admin.site.register(User)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "email", "username",
+                    "first_name", "last_name", "phone",
+                    "is_staff", "is_superuser",
+                    "is_verified", "is_active",
+                    "date_joined", "last_login", ]
+
+    list_filter = ["phone",
+                   "is_staff", "is_superuser",
+                   "is_verified", "is_active",
+                   "date_joined", "last_login", ]
+
+    search_fields = ["email", "username",
+                   "first_name", "last_name", "phone",
+                   "is_staff", "is_superuser",
+                   "is_verified", "is_active",
+                   "date_joined", "last_login", ]
