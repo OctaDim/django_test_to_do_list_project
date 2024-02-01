0fe15606f2eeecef810fa362fdab3eb2b770580b
Dima Mezhevich <octadim@gmail.com>
2024-02-02 02:09:31 +0300

    +feat: Add permissions decorators IsAuthenticated to the views functions

--- apps/api/views/views_functions.py
+++ apps/api/views/views_functions.py
@@ -7,15 +7,25 @@ from django.shortcuts import render  # Added
 from rest_framework.request import Request  # Added
 from rest_framework.response import Response  # Added
 from rest_framework import status  # Added
-from rest_framework.decorators import api_view  # Added
+
+from rest_framework.decorators import (permission_classes,  # DM Added
+                                       api_view,  # Added
+                                       )
+
+from rest_framework.permissions import (IsAuthenticated,  # DM Added
+                                        IsAdminUser,
+                                        AllowAny,
+                                        )
 
 from apps.api.serializers import TaskModelSerializer  # Added
+
 from apps.todo.models import Task  # Added
 
 
 # ##### VIEWS VIA FUNCTIONS (SAMPLES) USUALLY NOT USED IN PRACTICE #####
 
 @api_view(["GET"])  # Attention. Methods for this decorator should passed in !!!list format!!! The same as if request == "GET"
+@permission_classes([IsAuthenticated])  # DM
 def get_all_tasks(request: Request):  # Defined at once, that request is type Request
     # if request.method == "GET":     # Unnecessary, because defined in decorator
     tasks = Task.objects.all()
@@ -30,6 +40,7 @@ def get_all_tasks(request: Request):  # Defined at once, that request is type Re
 
 
 @api_view(["POST"])  # Attention. Methods for this decorator should passed in !!!list format!!! The same as if request == "POST"
+@permission_classes([IsAuthenticated])  # DM
 def create_new_task(request: Request):
     serializer = TaskModelSerializer(data=request.data)
 
