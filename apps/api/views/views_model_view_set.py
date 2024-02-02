6411e9e44a55cc4ae91ebbd30b7ddb245d402dbc
Dima Mezhevich <octadim@gmail.com>
2024-02-02 13:40:27 +0300

    +feat: Define permissions (permission classes)

--- apps/api/views/views_model_view_set.py
+++ apps/api/views/views_model_view_set.py
@@ -1,7 +1,6 @@
 # #################### Via ModelViewSet Classes ########################
 # CONTENT:
 #
-
 from rest_framework.viewsets import ModelViewSet  # Added
 
 from apps.api.serializers import (StatusModelSerializer,  # Added
@@ -10,12 +9,17 @@ from apps.api.serializers import (StatusModelSerializer,  # Added
 from apps.todo.models import (Status,  # Added
                               Category)
 
+from rest_framework.permissions import (IsAuthenticated,  # Added
+                                        IsAdminUser,
+                                        AllowAny)
 
 class StatusViewSet(ModelViewSet):  # All CRUD methods are under the hood included
     queryset = Status.objects.all()
     serializer_class = StatusModelSerializer
 
 
-class CategoryViewSet(ModelViewSet):  # All CRUD methods are under the hood included
+class CategoryViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    # All CRUD methods are under the hood included
     queryset = Category.objects.all()
     serializer_class = CategoryModelSerializer
