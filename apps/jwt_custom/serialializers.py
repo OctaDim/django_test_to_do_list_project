5df960d1e372bc15b2683381aefbc2f5e6e3cc9a
Dima Mezhevich <octadim@gmail.com>
2024-02-03 22:42:04 +0300

    +feat: CustomTokenObtainPairSerializer was added with additional fields email, user_first_name, user_last_name

---
+++ apps/jwt_custom/serialializers.py
@@ -0,0 +1,19 @@
+from rest_framework_simplejwt.serializers import (
+    TokenObtainPairSerializer,
+    AuthUser, )
+
+from rest_framework_simplejwt.tokens import Token
+from apps.user.models import User
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user: AuthUser) -> Token:
+        token = super().get_token(user=user)
+
+        token["email"] = user.email
+        # token["username"] = user.username
+        token["user_first_name"] = user.first_name
+        token["user_last_name"] = user.last_name
+
+        return token
