ab48bbc0208a162c367436d1b5ff63d27801e572
Dima Mezhevich <octadim@gmail.com>
2024-02-03 23:16:53 +0300

    +feat: Additional keys (username, user_full_name(from @property)) were added to the CustomTokenObtainPairSerializer

--- apps/jwt_custom/serializers.py
+++ apps/jwt_custom/serializers.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.serializers import (
     AuthUser, )
 
 from rest_framework_simplejwt.tokens import Token
+from apps.user.models import User
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -11,8 +12,9 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user=user)
 
         token["email"] = user.email
-        # token["username"] = user.username
+        token["username"] = user.username
         token["user_first_name"] = user.first_name
         token["user_last_name"] = user.last_name
+        token["user_full_name"] = user.get_full_name
 
         return token
