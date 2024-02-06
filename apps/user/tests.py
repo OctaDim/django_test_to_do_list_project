36d33c0d80b1ccc6b4e3bb2ccd791f45e926fa25
Dima Mezhevich <octadim@gmail.com>
2024-02-06 04:22:47 +0300

    +tests: Added different tests for the view AppsUserListUsersExceptCurrentGenericList

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -9,10 +9,12 @@ from apps.user.models import User
 
 
 
-class AppsUserListUsersGenericList(TestCase):
+class AppsUserListUsersExceptCurrentGenericListTest(TestCase):
     # check, if permissions is working (admin, not admin)
     # check, if data displaying (every field is corresponding to the field type)
     # check, that returns empty list of the users, if no users
+    datetime_regex = re.compile(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d+Z')  # 2024-02-02 15:02:33.874252+03
+
     def setUp(self):
         # defining settings for creating fake test User before executing test case
         self.user = User.objects.create_user(
@@ -34,12 +36,51 @@ class AppsUserListUsersGenericList(TestCase):
             is_superuser=True,
             )
 
-        # imitate front client, as if any front client (user) makes requests
-        self.client = APIClient()
+        self.client = APIClient()  # imitate front client, as if any front client (user) makes requests
 
 
-    # def get_all_users_by_admin(self):
-    #     self.client.force_authenticate()  # Authorises fake user by force in our fake test system
-    #
-    #     response = self.client.get("http://127.0.0.1:8000/api/generics/user/all_users-v2/")
-    #     assert
+    def test_get_users_as_admin(self):
+        self.client.force_authenticate(user=self.admin)  # Authorises fake user by force in our fake test system
+        response = self.client.get("http://127.0.0.1:8000/api/generics/user/all_users-v2/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Check if status is status.HTTP_200_OK
+        self.assertIsInstance(response.data["data"], list)  # Check if type is dict
+        self.assertEqual(len(response.data["data"]), User.objects.count() - 1)  # Check if count=User.objects.count()-1
+        # print("##### TEST DATA #####", response.data["data"])
+        # print("##### TEST DATA #####", len(response.data["data"]))
+        # print("##### TEST DATA #####", User.objects.count() - 1)
+
+
+        for each_user in response.data["data"]:  # for user in response.data["data"] because I return data = "message" and "data" as keys
+        # for user in response.data:  # for user in response.data should be if return in data = serializer.data only
+            self.assertIsInstance(each_user['email'], str)  # Check if type is str
+            self.assertIsInstance(each_user["first_name"], str)  # Check if type is str
+            self.assertIsInstance(each_user["last_name"], str)  # Check if type is str
+            self.assertIsInstance(each_user["username"], str)  # Check if type is str
+            self.assertIsInstance(each_user["phone"], (str | None))  # Check if type is str or None
+
+            self.assertIsInstance(each_user["is_staff"], bool)  # Check if type is bool
+            self.assertIsInstance(each_user["is_superuser"], bool)  # Check if type is bool
+            self.assertIsInstance(each_user["is_verified"], bool)  # Check if type is bool
+            self.assertIsInstance(each_user["is_active"], bool)  # Check if type is bool
+
+            self.assertTrue(
+                self.datetime_regex.match(each_user["date_joined"]), datetime)
+
+            self.assertTrue(
+                self.datetime_regex.match(each_user["last_login"]), datetime)
+
+
+    def test_get_users_as_non_admin(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get("http://127.0.0.1:8000/api/generics/user/all_users-v2/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    def test_get_no_users(self):
+        self.client.force_authenticate(user=self.admin)
+        User.objects.all().delete()
+        response = self.client.get("http://127.0.0.1:8000/api/generics/user/all_users-v2/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertIsInstance(response.data["data"], list)  # for user in response.data["data"] because I return data = "message" and "data" as keys
+        # self.assertIsInstance(response.data, list)  # for user in response.data should be if return in data = serializer.data only
